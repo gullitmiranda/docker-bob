be690a59e562b9bef4529c1f784dd4319a72695c
Luis Baroni <lcsbaroni@yahoo.com.br>
2014-11-30 17:52:10 -0200

    Config docker bob

--- start-container.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run -d --name apache -p 80:80 -v $1:/app -v $2:/sites-available -v /home/lg/docker/apache/logs:/logs --link mysql:mysql --link memcached:memcached --link solr:solr tutum/apache-php
